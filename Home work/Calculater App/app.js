7eb610af91a6a71f64bfd89664597a5281b1b2bd
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-10-26 13:58:21 +0500

    Calculater added

---
+++ Home work/Calculater App/app.js
@@ -0,0 +1,38 @@
+var sign="";
+var value1="";
+var value2=""; 
+// function getNumber(value)
+// {   
+// var result = document.getElementById("in");
+// if(value===[0-9] && value1==="")
+// {
+// value1=value;
+// result+=value1;
+// }
+// else if(value===[0-9] && value2==="")
+// {
+// value2=value;
+// result+=value2;
+// }
+// else if(value==="*"||value==="/"||value==="+"||value==="-")
+// {
+// sign=value;
+// result+=sign;
+// }
+
+// }
+
+
+function getNumber(num)
+{
+    
+var result=document.getElementById('result');
+result.value+=num
+}
+
+
+function solve()
+{
+    var result=document.getElementById('result');
+    result.value=eval(result.value)
+}
