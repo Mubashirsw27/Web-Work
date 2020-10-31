a1d484409aa4af48be7c6a1d7c9ea93bd0d504f6
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-10-31 13:41:33 +0500

    Assignment Updated

---
+++ Home work/Assignment/chapter 9-11/task 1-4/app.js
@@ -0,0 +1,38 @@
+//Task 1
+
+// var city =prompt("Enter City Name");
+// if(city==="Karachi")
+// {
+//     document.write("Welcome to the City of Lights");
+// }
+
+//Task 2
+
+// var gender = prompt("Enter your Gender : ");
+// if(gender==="male")
+// {
+//     document.write("Good morning Sir");
+// }
+// else{
+//     document.write("Good morning Maam");    
+// }
+
+//Task 3
+
+// var lightColor = prompt("Enter Light Color : ");
+// if (lightColor === "red") {
+//     document.write("Must Stop");
+// }
+// else if (lightColor === "yellow") {
+//     document.write("Ready to move");
+// }
+// else {
+//     document.write("Move now");
+// }
+
+//Task 4
+
+var litre = +prompt("Enter your Remaining Fuel : ");
+if (litre <= 0.25) {
+    document.write("Please refill the fuel in your car.");
+}
