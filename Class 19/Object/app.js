68946d87a9f9cb82dfa52ff26001f67c82a32789
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-12-19 13:48:26 +0500

    Work added

---
+++ Class 19/Object/app.js
@@ -0,0 +1,14 @@
+//*****Object*****
+
+// var webcourse = {// For Creating an Object
+//     name : "Mubashir",
+//     fatherName: "Ibrahim",
+//     fullName : ""
+// };
+// console.log(webcourse.fullName);
+
+// var course = {//we can create an empty Object also
+// };
+
+course.class = []
+console.log(course)
