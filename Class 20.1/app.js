ed45ee24f7796925e04469b586ab4e445275631c
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-12-21 11:22:54 +0500

    Class 20.1 added

---
+++ Class 20.1/app.js
@@ -0,0 +1,29 @@
+// function Student(name,roll)//Constructor //used for creating Objects easily.
+// {
+//     this.name=name;
+//     this.roll=roll;
+//     this.getName=function()
+//     {
+//         return name;
+//     }
+// }
+
+var stud = {
+   name:"Mubi" ,
+   roll:12
+}
+// var stud1=new Student("Mubashir",27);
+// var stud2=new Student("Babr",30);
+
+// console.log(stud1.getName());
+// console.log(stud2);
+// console.log("roll" in stud);
+// for (var a in stud)
+// {
+//  console.log(a);
+// }
+//  for(var a in stud)
+//  {
+//      console.log(stud[a]);
+//  }
+//  console.log(stud.hasOwnProperty("roll"));
