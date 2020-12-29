bd16a10c9e96b1bf0fd54c23b78e2fb8a2e925f0
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-12-29 13:36:22 +0500

    Work added

---
+++ Learned from W3schools/JS errors/app.js
@@ -0,0 +1,45 @@
+// //The try and catch for catching any error in the program
+
+// try{
+//     const a=5;
+//     a=5
+// }
+// catch(err)
+// {
+// console.log(err.message); //The err has two properties which are name & message.
+// }
+
+//The throw key word
+
+// try {
+//     var a = prompt("A");
+//     if (isNaN(a)) throw "Bitch its not a number";
+// } catch (error) {
+//  console.log(error);
+// }
+
+//The Finally Keyword
+
+// try {
+// const a=5;
+// a=1   
+// } 
+// catch (error) {
+//    console.log(error) 
+// }
+// finally
+// {
+// console.log("hal re lossi")
+// }
+
+//The JS Hoisting
+
+// car="Ferari"
+// console.log(car)
+// const car;
+
+//Strict Mode In JS
+
+// "use strict";
+// var implements="Fer"
+// console.log(car)
