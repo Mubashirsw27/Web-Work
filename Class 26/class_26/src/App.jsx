bec209e23daf2abea01a5932ad7b051a7348a8a2
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2021-01-10 17:49:06 +0500

    React JS updated

--- Class 26/class_26/src/App.jsx
+++ Class 26/class_26/src/App.jsx
@@ -18,7 +18,7 @@ class App extends Component {
       <div className='header'>
         <Header />
 
-        <h2 style={{ color: "red", fontSize: '50px' }}>World</h2>
+        <h2 style={{ color: "red", fontSize: '20px' }}>World</h2>
         <img src="https://media.wired.com/photos/5d09594a62bcb0c9752779d9/125:94/w_1994,h_1500,c_limit/Transpo_G70_TA-518126.jpg" width='300px' alt="" />
         <Footer />
       </div>
