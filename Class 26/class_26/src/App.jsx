ecb2c9ab3589c15ea276a9f505983781551832c6
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2021-01-10 17:36:17 +0500

    React JS updated

--- Class 26/class_26/src/App.jsx
+++ Class 26/class_26/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
 import React, { Component } from 'react';
-function Header(){
-  return(
-    <h1>Hello</h1>
-  );
-}
+
+
+// function Header(){
+//   return(
+//     <h1>Hello</h1>
+//   );
+// }
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
     <Header />
 
     <h2 style={{color:"red" ,fontSize:'50px'}}>World</h2>
+    <img src="https://media.wired.com/photos/5d09594a62bcb0c9752779d9/125:94/w_1994,h_1500,c_limit/Transpo_G70_TA-518126.jpg" width='300px' alt=""/>
     </div>
     );
   }
