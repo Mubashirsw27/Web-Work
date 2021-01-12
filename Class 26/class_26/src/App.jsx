53534719cc29f4177237b45c2acf5996fcdcf6d1
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2021-01-12 13:41:11 +0500

    React JS updated

--- Class 26/class_26/src/App.jsx
+++ Class 26/class_26/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TextField from './components/TextField';
 
 
 // function Header(){
@@ -17,7 +18,8 @@ class App extends Component {
     return (
       <div className='header'>
         <Header />
-
+        <TextField/>
+        <button type="button" className="btn btn-warning">Warning</button>
         <h2 style={{ color: "red", fontSize: '30px' }}>World</h2>
         <img src="https://media.wired.com/photos/5d09594a62bcb0c9752779d9/125:94/w_1994,h_1500,c_limit/Transpo_G70_TA-518126.jpg" width='300px' alt="" />
         <Footer />
